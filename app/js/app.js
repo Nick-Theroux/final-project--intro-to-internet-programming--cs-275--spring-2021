6feec88a554c83a645f045856cc8984e0d8bc880
Nicholas Theroux <78562198+Nick-Theroux@users.noreply.github.com>
2021-05-12 14:59:15 -0400

    Created Move diamond Function
    This function moves the diamond from one end of the viewport to another and back again.

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,13 @@
 window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
+        
         let size = window.prompt("Enter the size of the diamond: ")
         
         let object = document.querySelector('div')
+        
+        let left = 0
+        let right = 0
         
         function generate(size){
             let text = "" 
@@ -14,7 +18,7 @@ window.onload = () => {
                     }
                     text = text + "\n"
                     object.innerHTML = text
-                    object.style.textAlign = "center";
+                    object.style.textAlign = "center"
                 }
                 for (let i=size; i>=0; i--){
                     for (let j=0; j<=i; j++){
@@ -22,7 +26,7 @@ window.onload = () => {
                     }
                     text = text + "\n"
                     object.innerHTML = text
-                    object.style.textAlign = "center";
+                    object.style.textAlign = "center"
                 }
             }
             else{
@@ -32,7 +36,7 @@ window.onload = () => {
                     }
                     text = text + "\n"
                     object.innerHTML = text
-                    object.style.textAlign = "center";
+                    object.style.textAlign = "center"
                 }
                 for (let i=size; i>=0; i--){
                     for (let j=0; j<=i; j++){
@@ -40,10 +44,25 @@ window.onload = () => {
                     }
                     text = text + "\n"
                     object.innerHTML = text
-                    object.style.textAlign = "center";
+                    object.style.textAlign = "center"
                 }
             }
         }
+        let moveDiamond = () =>{
+            if (left < (window.innerWidth - object.clientWidth + 6) && right == 0){
+                object.style.left = left + 'px'
+                left += 1
+            }
+            else if(left != 0){
+                right = 1
+                object.style.left = left + 'px'
+                left -= 1
+                if (left == 0)
+                    right = 0
+            }
+        }
+        
         generate(size)
+        setInterval(moveDiamond, 5)
 
 };
