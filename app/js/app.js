aaccab20cc6004a1a8a9fae6942541ccf3dd866d
Nicholas Theroux <78562198+Nick-Theroux@users.noreply.github.com>
2021-05-12 12:15:33 -0400

    Built the top of the diamond
    This code generates the top half of the diamond.

--- app/js/app.js
+++ app/js/app.js
@@ -2,22 +2,31 @@ window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
         let size = window.prompt("Enter the size of the diamond: ")
-        function generate(size){
+        
+        let object = document.querySelector('div')
+        
+        function generateTop(size){
+            let text = ""
             if (size %2==0){
                 for (let i=0; i<size; i++){
-                    for (let j=0; j<i; j++){
-                        let mid = "* " 
+                    for (let j=0; j<=i; j++){
+                        text += "* "
                     }
+                    text = text + "\n"
+                    object.innerHTML = text
+                    object.style.textAlign = "center";
                 }
             }
             else{
                 for (let i=0; i<size; i++){
-                    for (let j=0; j<i; j++){
-                        let mid = "* " 
-
+                    for (let j=0; j<=i; j++){
+                        text += "*"
                     }
+                    text = text + "\n"
+                    object.innerHTML = text
+                    object.style.textAlign = "center";
                 }
             }
         }
-        generate(size)
+        generateTop(size)
 };
