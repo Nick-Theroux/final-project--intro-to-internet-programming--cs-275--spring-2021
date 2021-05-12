b955564c54db4f9ef4f2656e71dc6d31a02a4053
Nicholas Theroux <78562198+Nick-Theroux@users.noreply.github.com>
2021-05-12 02:09:57 -0400

    Added Generate function
    Starts to generate the diamond based on user input. **changes based on even or odd numbers**

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,23 @@
 window.onload = () => {
     console.log(`%c JavaScript works!`,
         `background-color: yellow; font-weight: bold;`);
+        let size = window.prompt("Enter the size of the diamond: ")
+        function generate(size){
+            if (size %2==0){
+                for (let i=0; i<size; i++){
+                    for (let j=0; j<i; j++){
+                        let mid = "* " 
+                    }
+                }
+            }
+            else{
+                for (let i=0; i<size; i++){
+                    for (let j=0; j<i; j++){
+                        let mid = "* " 
+
+                    }
+                }
+            }
+        }
+        generate(size)
 };
